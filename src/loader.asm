4d73a6a5888e8bf19c687186cd08dafa169cdfaa
qinqon <quique.llorente@gmail.com>
2015-12-27 17:59:06 +0100

    Call c function kmain from assambler

--- src/loader.asm
+++ src/loader.asm
@@ -1,9 +1,16 @@
 global loader                    ; the entry symbol for ELF
+extern kmain
+
 
 MAGIC_NUMBER equ 0x1BADB002      ; define the magic number constant
 FLAGS        equ 0x0             ; multiboot flags
 CHECKSUM     equ -MAGIC_NUMBER   ; calculate the checksum
+KERNEL_STACK_SIZE equ 4096       ; size of stack in bytes
                                  ; (magic number + checksum + flags should equal 0)
+section .bss
+align 4                          ; align at 4 bytes
+kernel_stack:                    ; label points to beginning of memory
+   resb KERNEL_STACK_SIZE        ; reserve stack for the kernel
 
 section .text:                   ; start of the text (code) section
 align 4                          ; the code must be 4 byte aligned
@@ -13,5 +20,9 @@ align 4                          ; the code must be 4 byte aligned
 
 loader:                          ; the loader label (defined as entry point in linker script)
    mov eax, 0xCAFEBABE           ; place the number 0xCAFEBABE in the register eax
+   mov esp, kernel_stack + KERNEL_STACK_SIZE
+                                 ; point esp to the start of the
+                                 ; stack (end of memory area)
+   call kmain                    
 .loop:
    jmp .loop                     ; loop forever
